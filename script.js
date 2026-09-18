0e8afce22041ba6fda31e3376db6539e56ec9ece
JUN <cosmos.official.nlcs@gmail.com>
2026-09-18 15:01:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,17 @@ const DATA = {
     { name: "Senior School Third Floor Map", url: "images/photos/map3.png" },
     { name: "Google Classroom", url: "https://classroom.google.com/h/st" }
   ],
-  "Skl stuff": [
+  "Isams": [
     { name: "School Calendar", url: "#" },
     { name: "Parent Portal", url: "#" }
   ],
-  "CCAs": [
+  "SOCS": [
     { name: "Cosmos Cloud Society List", url: "#" }
   ],
-  "Support": [
+  "Guides": [
+    { name: "Help & Technical Support", url: "#" }
+  ],
+  "Tech Support": [
     { name: "Help & Technical Support", url: "#" }
   ],
   "Parser for CAS": [
@@ -70,6 +73,16 @@ const PALETTES = [
     btnBg: '#f3e8fd',
     btnText: '#7627b5',
     iconSvg: `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>`
+  },
+  {
+    iconBg: '#d93025',        
+    activeBg: '#fce8e6',
+    activeText: '#c5221f',
+    cardBg: '#fdf5f5',
+    cardBorder: '#fad2cf',
+    btnBg: '#fce8e6',
+    btnText: '#c5221f',
+    iconSvg: `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/></svg>`
   },
   {
     iconBg: '#d93025',        
